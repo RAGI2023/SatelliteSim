1c76ebca2a5843e641739c0bfef2d8471647540e
JDHK01 <zhangyiqun@bupt.edu.cn>
2025-04-12 12:47:47 +0800

    bpsk,qpsk编码,规划卫星路径

--- BackEnd/bpsk_modulate.py
+++ BackEnd/bpsk_modulate.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 #bit_seq:比特序列   carrier_freq:载波频率   sample_rate:采样频率    symbol_duration:时间周期
-def bpsk_modulate(bit_seq, carrier_freq=100, sample_rate=1000, symbol_duration=0.01):
+def bpsk_modulate(bit_seq, carrier_freq=1000, sample_rate=10000, symbol_duration=0.01):
     t = np.arange(0, symbol_duration, 1/sample_rate)#一段时间序列
     modulated_signal = []
 
@@ -10,15 +11,3 @@ def bpsk_modulate(bit_seq, carrier_freq=100, sample_rate=1000, symbol_duration=0
         modulated_signal.extend(signal)
 
     return np.array(modulated_signal)
-
-if __name__ == "__main__":
-    # 模拟前端请求中的比特流
-    bitstream = np.random.randint(0, 2, 16)  # 16位比特
-    print("原始比特流:", bitstream)
-
-    # BPSK 调制
-    bpsk_signal = bpsk_modulate(bitstream)
-    print("BPSK 信号生成完毕，长度:", len(bpsk_signal))
-    print("BPSK 信号数据类型:", bpsk_signal.dtype)
-    for i in range(16):
-        print(bpsk_signal[i],sep=' ')
